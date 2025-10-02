851d64d14ce9b028ac73fbdcf3470fb0b3958157
bbaudry <baudry@kth.se>
2025-10-01 22:15:47 -0400

    set the gen art stage

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A reuseable action that generates a piece of visual art with nannou
 
+# acknowledgement
 
+Thank you @dbanty for the great post about [writing an action in rust](https://dylananthony.com/blog/how-to-write-a-github-action-in-rust/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use nannou::prelude::*;
+use nannou::color::*;
+
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+    nannou::sketch(view).run()
 }
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    let char_hue: HashMap<&str, i32> = HashMap::from([
+    ("0", 0),
+    ("1", 22),
+    ("2", 44),
+    ("3", 66),
+    ("4", 88),
+    ("5", 110),
+    ("6", 132),
+    ("7", 154),
+    ("8", 176),
+    ("9", 198),
+    ("a", 220),
+    ("b", 242),
+    ("c", 264),
+    ("d", 286),
+    ("e", 308),
+    ("f", 330),
+]);
+
+    draw.background().color(PURPLE);
+    draw.ellipse().color(hsl((char_hue["f"] as f32) / 360.0,1.0,0.5));
+    draw.to_frame(app, &frame).unwrap();
+}
+
