2adaf28de6798809ed54d1a171f098b87e2ec52f
bbaudry <baudry@kth.se>
2025-10-05 19:22:55 -0400

    adding knobs and inputs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use nannou::prelude::*;
 use nannou::color::*;
 use std::str;
 use std::collections::HashMap;
+use std::env;
 
 // interesting variables
 const CANVAS_W: u32 = 420; 
@@ -10,10 +11,42 @@ const HASH_LENGTH: f32 = 7.0;
 const STRIPE_W: f32 = CANVAS_W as f32 / HASH_LENGTH;
 
 fn main() {
-    nannou::sketch(view).size(CANVAS_W, CANVAS_H).run()
+    nannou::app(model)
+        .loop_mode(LoopMode::loop_ntimes(1))
+        //.update(update)
+        .view(view)
+        //.simple_window(view)
+        .run();
 }
 
-fn view(app: &App, frame: Frame) {
+struct Model {
+    commit_hash: String,
+    w:u32,
+    h:u32,
+    stripe_w:u32
+}
+
+fn model(app: &App) -> Model {
+    let args: Vec<String> = env::args().collect();
+    let h=&args[1];
+    let stripe_w:u32=60;
+    let canvas_w:u32=u32::try_from(h.len()).unwrap()*stripe_w;
+    let canvas_h:u32=200;
+    dbg!(h);
+    app.new_window()
+    .size(canvas_w,canvas_h)
+    .build()
+    .unwrap();
+    Model{
+        commit_hash:String::from(h),
+        w:canvas_w,
+        h:canvas_h,
+        stripe_w:stripe_w,
+    }
+}
+
+
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let char_hue: HashMap<&str, i32> = HashMap::from([
     ("0", 0),
@@ -34,10 +67,10 @@ fn view(app: &App, frame: Frame) {
     ("f", 330),
 ]);
     draw.background().color(PURPLE);
-    let s: &str = "fa42c98";
+    let s: &str = &model.commit_hash;
     // get the characters from the hash
     let char_vec: Vec<char> = s.chars().collect();
-    let mut x=- (CANVAS_W as f32/2.0) + STRIPE_W as f32 / 2.0;
+    let mut x=- (&model.w as f32/2.0) + &model.stripe_w as f32 / 2.0;
     for c in char_vec {
         // convert the character into a str: https://www.reddit.com/r/rust/comments/eanwkm/how_to_create_a_str_from_char/
         let i=c.to_string();
@@ -45,9 +78,9 @@ fn view(app: &App, frame: Frame) {
 
         draw.rect()
             .color(hsl((char_hue[j] as f32) / 360.0,1.0,0.5))
-            .w_h(STRIPE_W,CANVAS_H as f32)
+            .w_h(&model.stripe_w,&model.h as f32)
             .x_y(x,0.0);
-        x=x+STRIPE_W;
+        x=x+&model.stripe_w;
     }
 
     draw.to_frame(app, &frame).unwrap();
